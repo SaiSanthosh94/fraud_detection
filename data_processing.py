4d859de280391c708e9a144de7c4bc8aa3545265
SaiSanthosh94 <saisanthosh073@gmail.com>
2022-02-27 03:30:30 +0530

    aded a new funtion sum

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,13 @@
+from re import A
+
+
 data_processing.py
 
     print("Learing how to push python code to github ")
+
+    a=5
+    b=8
+    def sum(a,b):
+        c = a + b
+        return a
+    print ("aded a new funtion sum")
