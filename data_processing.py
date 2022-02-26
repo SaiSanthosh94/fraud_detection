e650538c05c01c251b0772c0b7ba96805a40031b
SaiSanthosh94 <saisanthosh073@gmail.com>
2022-02-27 04:07:56 +0530

    Revert "aded a new funtion sum"
    This reverts commit 4d859de280391c708e9a144de7c4bc8aa3545265.

--- data_processing.py
+++ data_processing.py
@@ -1,13 +1,3 @@
-from re import A
-
-
 data_processing.py
 
     print("Learing how to push python code to github ")
-
-    a=5
-    b=8
-    def sum(a,b):
-        c = a + b
-        return a
-    print ("aded a new funtion sum")
